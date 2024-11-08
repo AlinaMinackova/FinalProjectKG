7734bd2c6549ceb19f2669868fc8ab79ff1d7dbf
AlikaMinakova <minackowa.ali@yandex.ru>
2024-11-08 21:05:05 +0300

    create simple gui2.fxml file and GuiController for it

--- Simple3DViewer/src/main/java/com/cgvsu/GuiControllerTest.java
+++ Simple3DViewer/src/main/java/com/cgvsu/GuiControllerTest.java
@@ -1,29 +1,56 @@
 package com.cgvsu;
 
-import com.cgvsu.render_engine.RenderEngine;
-import javafx.fxml.FXML;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import javafx.stage.FileChooser;
-import javafx.util.Duration;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.io.IOException;
-import java.io.File;
-import javax.vecmath.Vector3f;
-
 import com.cgvsu.model.Model;
 import com.cgvsu.objreader.ObjReader;
 import com.cgvsu.render_engine.Camera;
+import com.cgvsu.render_engine.RenderEngine;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
-public class GuiController {
+import javax.vecmath.Vector3f;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.IllegalFormatCodePointException;
+import java.util.Objects;
 
-    final private float TRANSLATION = 0.5F; //шаг перемещения камеры
+public class GuiControllerTest {
+
+    final private float TRANSLATION = 0.9F; //шаг перемещения камеры
+
+    @FXML
+    public AnchorPane modelPane;
+
+    @FXML
+    public AnchorPane gadgetPane;
+
+    @FXML
+    private Button open;
+
+    @FXML
+    private Button save;
+
+    @FXML
+    private TextField text;
+
+    @FXML
+    void save(MouseEvent event) {
+
+    }
 
     @FXML
     AnchorPane anchorPane;
@@ -65,7 +92,7 @@ public class GuiController {
     }
 
     @FXML
-    private void onOpenModelMenuItemClick() {
+    void open(MouseEvent event) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Model (*.obj)", "*.obj"));
         fileChooser.setTitle("Load Model");
@@ -86,33 +113,57 @@ public class GuiController {
         }
     }
 
-    @FXML
-    public void handleCameraForward(ActionEvent actionEvent) {
+    public void handleCameraForward() {
         camera.movePosition(new Vector3f(0, 0, -TRANSLATION));
     }
 
     @FXML
-    public void handleCameraBackward(ActionEvent actionEvent) {
+    public void handleCameraBackward() {
         camera.movePosition(new Vector3f(0, 0, TRANSLATION));
     }
 
     @FXML
-    public void handleCameraLeft(ActionEvent actionEvent) {
+    public void handleCameraLeft() {
         camera.movePosition(new Vector3f(TRANSLATION, 0, 0));
     }
 
     @FXML
-    public void handleCameraRight(ActionEvent actionEvent) {
+    public void handleCameraRight() {
         camera.movePosition(new Vector3f(-TRANSLATION, 0, 0));
     }
 
     @FXML
-    public void handleCameraUp(ActionEvent actionEvent) {
+    public void handleCameraUp() {
         camera.movePosition(new Vector3f(0, TRANSLATION, 0));
     }
 
     @FXML
-    public void handleCameraDown(ActionEvent actionEvent) {
+    public void handleCameraDown() {
         camera.movePosition(new Vector3f(0, -TRANSLATION, 0));
+    }
+
+    public void moveModel(KeyEvent keyEvent) {
+        if (Objects.equals(keyEvent.getText(), "w")) {
+            handleCameraForward();
+        }
+        if (Objects.equals(keyEvent.getText(), "s")) {
+            handleCameraBackward();
+        }
+        if (Objects.equals(keyEvent.getText(), "a")) {
+            handleCameraLeft();
+        }
+        if (Objects.equals(keyEvent.getText(), "d")) {
+            handleCameraRight();
+        }
+        if (Objects.equals(keyEvent.getText(), "r")) {
+            handleCameraUp();
+        }
+        if (Objects.equals(keyEvent.getText(), "f")) {
+            handleCameraDown();
+        }
+    }
+
+    public void moveMouce(MouseEvent mouseEvent) {
+        //System.out.println("123");
     }
 }
